499a6c5092e4727980d558d7270642e7f3c0af13
chenlj <chenljscu@gmail.com>
2017-10-24 09:59:17 +0100

    for windows compile

---
+++ build_env.sh
@@ -0,0 +1,1 @@
+WINEPATH C:\\Program\ Files\\mingw-w64\\x86_64-7.2.0-posix-seh-rt_v5-rev0\\mingw64\\bin
--- include/daq.h
+++ include/daq.h
@@ -1,4 +1,5 @@
 
+#include <cstring>
 #include <string>
 #include <thread>
 #include <memory>

5e9bca4baa30113f5581b52c56b3ec07e1648f1b
chenlj <chenljscu@gmail.com>
2018-01-05 09:28:32 +0800

    VIsual Studio compile

--- build_env.bat
+++ build_env.bat
@@ -1,8 +1,10 @@
-set path=%path%;C:\Program Files\mingw-w64\x86_64-7.2.0-posix-seh-rt_v5-rev0\mingw64\bin
-set CC=C:\Program Files\mingw-w64\x86_64-7.2.0-posix-seh-rt_v5-rev0\mingw64\bin\gcc.exe
-set CXX=C:\Program Files\mingw-w64\x86_64-7.2.0-posix-seh-rt_v5-rev0\mingw64\bin\g++.exe
-set path=%path%;C:\Program Files\swigwin-3.0.12
+set PATH=%PATH%;C:\Program Files\swigwin-3.0.12
 
-set PYTHON_EXECUTABLE:FIFLEPATH=C:\Program Files\Python36
-set PYTHON_INCLUDE_DIR:PATH=C:\Program Files\Python36\include
-set PYTHON_LIBRARY:FILEPATH=C:\Program Files\Python36\python3.dll
+set PATH=%PATH%;C:\Program Files\Anaconda3
+set PYTHONPATH=%PYTHONPATH%;C:\Program Files\Anaconda3\libs
+set PYTHON_EXECUTABLE:FIFLEPATH=C:\Program Files\Anaconda3\python.exe
+set PYTHON_INCLUDE_DIR:PATH=C:\Program Files\Anaconda3\include
+set PYTHON_LIBRARY:FILEPATH=C:\Program Files\Anaconda3\python3.dll
+
+set VS90COMNTOOLS=%VS100COMNTOOLS%
+set DISTUTILS_USE_SDK=1

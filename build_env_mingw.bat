692234ae4417b7d4ea2edad6f88b8dfaba73b933
yiliu <yi.liu@desy.de>
2018-01-18 17:59:57 +0800

    code refactory: remove unused files

--- build_env_mingw.bat
+++
@@ -1,8 +0,0 @@
-set path=%path%;C:\Program Files\mingw-w64\x86_64-7.2.0-posix-seh-rt_v5-rev0\mingw64\bin
-set CC=C:\Program Files\mingw-w64\x86_64-7.2.0-posix-seh-rt_v5-rev0\mingw64\bin\gcc.exe
-set CXX=C:\Program Files\mingw-w64\x86_64-7.2.0-posix-seh-rt_v5-rev0\mingw64\bin\g++.exe
-set path=%path%;C:\Program Files\swigwin-3.0.12
-
-set PYTHON_EXECUTABLE:FIFLEPATH=C:\Program Files\Python36
-set PYTHON_INCLUDE_DIR:PATH=C:\Program Files\Python36\include
-set PYTHON_LIBRARY:FILEPATH=C:\Program Files\Python36\python3.dll

c02aff94e6665ed22893171d5137f615b9738f8e
YangTao314159 <2585434298@qq.com>
2018-01-19 22:21:05 +0800

    gui add hist

--- gui/JadePixelControl.py
+++ gui/JadePixelControl.py
@@ -10,8 +10,9 @@ matplotlib.use('Qt5Agg')
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5 import NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
+from matplotlib.animation import FuncAnimation
 
-import offlineAna
+import OfflineAnalysis
 
 
 form_class = uic.loadUiType("JadePixel.ui")[0]
@@ -20,6 +21,8 @@ class MainWindow(QtWidgets.QMainWindow, form_class):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
+
+        self.OfflineThread=OfflineAnalysis.OfflineThread()
         
         self.Draw_Pixel_Image()
     
@@ -34,36 +37,53 @@ class MainWindow(QtWidgets.QMainWindow, form_class):
         self.SpinBox_MinRow.valueChanged.connect(self.Mode_Update)
         self.SpinBox_MinCol.valueChanged.connect(self.Mode_Update)
         
-        self.LineEdit_CurrentData.setText(self.ComboBox_Data.currentText())
+        self.LineEdit_CurrentData.setText('???')
         
-        self.ComboBox_Data.activated.connect(self.update)
-        
-        offlineAna.Signal.FrameSignal.connect(self.ProcessData)
-        
-        self.update()
+        self.ComboBox_Data.activated.connect(self.LineEdit_CurrentData_Update)
 
+        self.Offline_Update()
+        
+        
 #####################################################################
 
     def Draw_Pixel_Image(self):
      
-        fig = Figure((4.0, 3.0))
+        fig = Figure((4.0, 8.0))
         fig.patch.set_visible(False)
         self.Pixel_Canvas = FigureCanvas(fig)
         self.Pixel_Canvas.setParent(self.Pixel_Graph)
         size = fig.get_size_inches()*fig.dpi
         self.Pixel_Graph.setMinimumSize(*size)
-        self.Pixel_Axes = fig.add_subplot(111)
-
+        self.Pixel_Axes = fig.add_subplot(211)
         zeros = np.zeros(shape = (16,48))
         self.Pixel_Image = self.Pixel_Axes.imshow(zeros, animated = True, interpolation = 'nearest')
-        #self.Pixel_Canvas.colorbar(self.Pixel_Image, orientation='vertical', format='%.0e')
+        
+        self.Pixel_Axes_hist = fig.add_subplot(212)
+        zeros_hist = np.zeros(shape=(1,10))
+        #self.Pixel_Hist_Image= self.Pixel_Axes_hist.hist(zeros_hist, 20, animated = True, histtype='stepfilled')
+
         self.Pixel_Canvas.draw()
         Pixel_mpl_toolbar = NavigationToolbar(self.Pixel_Canvas, self.Pixel_Graph)
         
 
     def Btn_StartRun_Clicked(self, arg):
-        #self.Btn_StartRun.setAttribute(QtCore.Qt.WA_UnderMouse, False)
-        offlineAna.readRawFile(self.LineEdit_CurrentData.text(), 1, 5)
+        self.Btn_StartRun.setEnabled(False)
+        self.ComboBox_Data.setEnabled(False)
+
+
+        self.OfflineThread.fname=self.LineEdit_CurrentData.text()
+        self.OfflineThread.struceture=self.SpinBox_DataStructure.value()
+        self.OfflineThread.maxframe=self.SpinBox_MaxFrame.value()
+
+        frame = np.zeros(shape=(16,48))
+        self.Pixel_Image.set_data(frame)
+        self.Pixel_Image.autoscale()
+        self.Pixel_Axes.draw_artist(self.Pixel_Image)
+        self.Pixel_Canvas.update()
+
+        self.OfflineThread.start()
+
+
         
 
     def Btn_StopRun_Clicked(self):
@@ -102,19 +122,36 @@ class MainWindow(QtWidgets.QMainWindow, form_class):
             self.SpinBox_MaxRow.setEnabled(False)
             self.SpinBox_MaxCol.setEnabled(False)
             
-        
-    def update(self):
-        self.Mode_Update()
+    def LineEdit_CurrentData_Update(self):
         self.LineEdit_CurrentData.setText(self.ComboBox_Data.currentText())
-        
-    def ProcessData(self, data):
+
+    def Offline_Update(self):
+        self.OfflineThread.SendOver.connect(self.Offline_Btn_Update)
+        self.OfflineThread.SendOneFrame.connect(self.Frame_Update)
+
+
+    def Offline_Btn_Update(self):
+        self.Btn_StartRun.setEnabled(True)
+        self.ComboBox_Data.setEnabled(True)
+
+
+
+    def Offline_Image_Update(self):
         #print(np.reshape(data,  (48, 16)))
-        self.Pixel_Image.set_data(np.reshape(data, (16, 48)))
-        
-        self.Pixel_Image.autoscale()
-            
+        self.Pixel_Image.set_data(np.reshape(self.OfflineThread.frame, (16, 48))) 
+        print(self.OfflineThread.PixelADC)
+        #self.Pixel_Hist_Image= self.Pixel_Axes_hist.hist(self.OfflineThread.PixelADC, 20, animated = True, histtype='stepfilled')
+        #print(self.OfflineThread.frame)     
+        self.Pixel_Image.autoscale()          
         self.Pixel_Axes.draw_artist(self.Pixel_Image)
-        #self.Pixel_Canvas.update()
+        self.Pixel_Canvas.update()
+
+    def Lable_Update(self):
+        self.Label_EventNumber.setText(str(self.OfflineThread.frameId))
+
+    def Frame_Update(self):
+        self.Offline_Image_Update()
+        self.Lable_Update()
         
     
 

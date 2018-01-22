87366a07f3e4052f133a6b773b79959239a62b01
yiliu <yi.liu@desy.de>
2018-01-22 20:32:40 +0800

    refactory: split code file

--- software/lib/core/include/daq.h
+++ software/lib/core/include/daq.h
@@ -1,3 +1,5 @@
+#ifndef JADEPIX_INCLUDED_DAQ_H
+#define JADEPIX_INCLUDED_DAQ_H
 
 #include <cstring>
 #include <string>
@@ -5,54 +7,51 @@
 #include <memory>
 #include <atomic>
 
+class DataTakingThread;
+
 enum RunState {
-    STATE_STOPPED,
-    STATE_RUNNING,
-	// stop command issued, but thread hasn't stopped yet (e.g. hanging on read)
-	STATE_STOPPING,
+  STATE_STOPPED,
+  STATE_RUNNING,
+  STATE_STOPPING,
 };
 
 enum LogLevel {
-    LOG_DEBUG = 1,
-    LOG_INFO = 2,
-    LOG_WARNING = 3,
-    LOG_ERROR = 4,
+  LOG_DEBUG = 1,
+  LOG_INFO = 2,
+  LOG_WARNING = 3,
+  LOG_ERROR = 4,
 };
 
 class EventListener {
-public:
-    virtual void logMessage(LogLevel level, const char *string) = 0;
-    virtual ~EventListener() {};
+ public:  
+  virtual void logMessage(LogLevel level, const char *string) = 0;
+  virtual ~EventListener() {};
 };
-
-class DataTakingThread;
 
 class DataTaker {
-friend class DataTakingThread;
-public:
-    DataTaker(EventListener* listener);
-	~DataTaker();
-    void start_run();
-    void stop_run();
-    void stop_run_and_join();
-    RunState get_state();
-    int get_event_number();
-    int get_run_number();
-    size_t get_bytes_read();
-    // get_accumulated_events
-    // get_event_number
-    // get_run_number
+  friend class DataTakingThread;
+ public:
+  DataTaker(EventListener* listener);
+  ~DataTaker();
+  void start_run();
+  void stop_run();
+  void stop_run_and_join();
+  RunState get_state();
+  int get_event_number();
+  int get_run_number();
+  size_t get_bytes_read();  
+  void getRecentEvent(char *data);
+  const std::string& get_filename();
+  void set_filename( const std::string &fname );
 
-	void getRecentEvent(char *data);
-	
-	const std::string& get_filename(){ return m_filename; }
-	void set_filename( const std::string &fname ){ m_filename = fname; }
+ private:
+  void reportThreadStopped();
 
-private:
-    EventListener* m_listener;
-    std::atomic<RunState> m_state;
-    std::unique_ptr<DataTakingThread> m_threadObj;
-
-	void reportThreadStopped();
-	std::string m_filename;
+ private:
+  EventListener* m_listener;
+  std::atomic<RunState> m_state;
+  std::unique_ptr<DataTakingThread> m_threadObj;
+  std::string m_filename;
 };
+
+#endif
--- software/lib/core/include/daq_private.h
+++ software/lib/core/include/daq_private.h
@@ -1,3 +1,5 @@
+#ifndef JADEPIX_INCLUDED_DAQ_PRIVATE_H
+#define JADEPIX_INCLUDED_DAQ_PRIVATE_H
 
 #include <atomic>
 #include <thread>
@@ -5,46 +7,29 @@
 
 class EventListener;
 
-// see https://rafalcieslak.wordpress.com/2014/05/16/c11-stdthreads-managed-by-a-designated-class/
 class DataTakingThread {
-public:
-    DataTakingThread(DataTaker *dataTaker):
-        m_dataTaker(dataTaker), m_listener(dataTaker->m_listener), m_thread(), m_stop(false), m_eventNumber(0), m_runNumber(0), m_bytesRead(0) {};
+ public:
+  DataTakingThread(DataTaker *dataTaker);
+  ~DataTakingThread();
+  void start();
+  void stop();
+  void stopAndJoin();
 
-    ~DataTakingThread() {
-        stopAndJoin();
-    }
+ private:  
+  void threadMain();
 
-    void start() {
-        // This will start the thread. Notice move semantics!
-        m_thread = std::thread(&DataTakingThread::threadMain, this);
-    }
-
-	void stop() {
-		m_stop = true;
-	}
-
-    void stopAndJoin() {
-        m_stop = true;
-        if (m_thread.joinable()) m_thread.join();
-    }
-
-    // Todo: rather than atomic, use a regular int and locks.
-    // Event number is frequently updated but rarely read, and
-    // this might improve speed
-    std::atomic<int> m_eventNumber;
-	std::atomic<int> m_runNumber;
-    std::atomic<size_t> m_bytesRead;
-
-	// A recent event for display.  Must be accessed via a lock.
-	char m_recentEvent[1928] = {0};
-	std::mutex eventDataMutex;
-
-private:
-	DataTaker *m_dataTaker;
-    EventListener *m_listener;
-    std::thread m_thread;
-    std::atomic<bool> m_stop;
-
-    void threadMain();
+ public:
+  std::atomic<int> m_eventNumber;
+  std::atomic<int> m_runNumber;
+  std::atomic<size_t> m_bytesRead;
+  char m_recentEvent[1928];
+  std::mutex eventDataMutex;
+  
+ private:
+  DataTaker *m_dataTaker;
+  EventListener *m_listener;
+  std::thread m_thread;
+  std::atomic<bool> m_stop;
 };
+
+#endif

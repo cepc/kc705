4f4e2709f420be860e8f329833c5f261654f9043
yiliu <yi.liu@desy.de>
2018-02-06 13:54:44 +0800

    Test Utils: add help message, print argv

--- software/exe/src/ManagerTest.cxx
+++ software/exe/src/ManagerTest.cxx
@@ -8,11 +8,13 @@
 using namespace std::chrono_literals;
 
 int main(int argc, char **argv){
+  std::cout<<"options: -i <input_data_path> -o <output_data_path> -r <register_path> -s <run_time_milliseconds>"<<std::endl;
+  
   std::string opt_data_input = "//./xillybus_read_32";
   std::string opt_reg = "//./xillybus_mem_8";
   std::string opt_data_output = "output";
   std::string opt_time_run = "60";
-
+  
   for(int i = 1; i < argc; i++){
     std::string opt(argv[i]);
     if(opt=="-i"){
@@ -40,7 +42,7 @@ int main(int argc, char **argv){
       }
     }
   }
-
+  
   size_t time_run = std::stoul(opt_time_run);
   
   std::time_t time_now = std::time(nullptr);
@@ -50,12 +52,19 @@ int main(int argc, char **argv){
 		"%y%m%d%H%M%S", std::localtime(&time_now));
   std::string time_str(time_buff);
   std::string data_output_path = opt_data_output+"_"+time_str +".df";
+
+
+  std::cout<< "{opt_data_input:"<<opt_data_input<<"}"<<std::endl;
+  std::cout<< "{opt_reg:"<<opt_reg<<"}"<<std::endl;
+  std::cout<< "{data_output_path:"<<data_output_path<<"}"<<std::endl;
+  std::cout<< "{time_run:"<<time_run<<"}"<<std::endl;
   
   auto preg = new JadeRegCtrl(opt_reg);
   preg->WriteByte(4, 15);
   std::this_thread::sleep_for(100ms);
   preg->WriteByte(3, 15);
   auto pman = new JadeManager();
+  
   pman->Start(opt_data_input, data_output_path);
   preg->WriteByte(4, 15);
   std::this_thread::sleep_for(std::chrono::milliseconds(time_run));

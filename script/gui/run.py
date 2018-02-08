6ad834d058c1191a0141d0fddec6568b92966a84
chenlj <chenljscu@gmail.com>
2018-02-08 20:09:14 +0800

    ignore filter print

--- script/gui/run.py
+++ script/gui/run.py
@@ -3,34 +3,35 @@ import time
 
 def run(infile, outfile, regfile, run_time):
     
-    command1 = "ManagerTest "
+    command1 = "D:/code/JadePixDAQ/kc705/bin/ManagerTest.exe "
     command2 = "-r " + regfile + "  "
     command3 = "-i " + infile  + "  "
     command4 = "-o " + outfile + "  "
-    command5 = "-s " + str(run_time)
+    command5 = "-s " + str(run_time) + " "
+    command6 = "-p " + str(10000) + " "
 
-    command = command1 + command2 + command3 + command4 + command5
+    command = command1 + command2 + command3 + command4 + command5 + command6
 
     os.system(command)
 
 
 def sim_take(run_time, sleep_time, n):
-    infile = "../../data/2017-12-23-1.dat" 
-    outfile = "/tmp/test_out" 
-    regfile = "/tmp/test_reg"
+    infile = "D:/code/JadePixDAQ/kc705/data/2017-12-23-1.dat" 
+    outfile = "D:/code/JadePixDAQ/kc705/data/tmp/output/test" 
+    regfile = "D:/code/JadePixDAQ/kc705/data/tmp/test_reg"
     run_time = run_time * 1000 #s
     for i in range(0, n):
         run(infile, outfile, regfile, run_time)
         time.sleep(sleep_time)
 
-def win_take(run_time, n):
+def win_take(run_time, sleep_time, n):
     infile = "//./xillybus_read_32" 
     outfile = "output" 
     regfile = "//./xillybus_mem_8"
     run_time = run_time * 1000 #s
     for i in range(0, n):
         run(infile, outfile, regfile, run_time)
-        time.sleep(1)
+        time.sleep(sleep_time)
 
 if __name__ == "__main__":
-    sim_take(10, 1, 10)
+    sim_take(10, 2, 10)

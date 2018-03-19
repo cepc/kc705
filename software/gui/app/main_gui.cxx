d0c6cde5ac972fd3d09c356626c166aeaa51edb5
YangTaoGit <yangtao.cn@outlook.com>
2018-03-19 10:24:56 +0800

    add SplashScreen

--- software/gui/app/main_gui.cxx
+++ software/gui/app/main_gui.cxx
@@ -1,11 +1,17 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QSplashScreen> 
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-
+    QSplashScreen *splash = new QSplashScreen; 
+    splash->setPixmap(QPixmap("../software/gui/app/splash.png"));  
+    splash->show(); 
+    splash->showMessage("loading...",Qt::AlignRight | Qt::AlignBottom);
+    MainWindow *w = new MainWindow;
+    splash->finish(w); 
+    w->show();
+    delete splash;
     return a.exec();
 }

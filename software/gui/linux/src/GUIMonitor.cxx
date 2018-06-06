e5db47bcfd6647823dfa3e3ec263fea3080be2cf
Chen Liejian <chenlj@ihep.ac.cn>
2018-06-06 16:03:23 +0800

    new file:   linux/CMakeLists.txt 	new file:   linux/app/main_gui.cxx 	new file:   linux/form/iheplogo.gif 	new file:   linux/form/mainwindow.ui 	new file:   linux/include/GUIManager.hh 	new file:   linux/include/GUIMonitor.hh 	new file:   linux/include/QRootCanvas.hh 	new file:   linux/include/mainwindow.h 	new file:   linux/src/GUIManager.cxx 	new file:   linux/src/GUIMonitor.cxx 	new file:   linux/src/QRootCanvas.cxx 	new file:   linux/src/mainwindow.cpp 	new file:   windows/CMakeLists.txt 	new file:   windows/app/main_gui.cxx 	new file:   windows/form/iheplogo.gif 	new file:   windows/form/mainwindow.ui 	new file:   windows/include/GUIManager.hh 	new file:   windows/include/GUIMonitor.hh 	new file:   windows/include/mainwindow.h 	new file:   windows/include/qcustomplot.h 	new file:   windows/qrc/input.txt 	new file:   windows/qrc/mainwindow.qrc 	new file:   windows/src/GUIManager.cxx 	new file:   windows/src/GUIMonitor.cxx 	new file:   windows/src/mainwindow.cpp 	new file:   windows/src/qcustomplot.cpp

---
+++ software/gui/linux/src/GUIMonitor.cxx
@@ -0,0 +1,75 @@
+#include "GUIMonitor.hh"
+
+using namespace std::chrono_literals;
+
+GUIMonitor::GUIMonitor(const JadeOption& options)
+    : JadeMonitor(options)
+    , m_opt(options)
+    , m_ev_get(0)
+    , m_ev_num(0)
+    , m_nx(16)
+    , m_ny(48)
+    , m_col(0)
+    , m_row(0)
+    , m_thr(0) //*
+{
+  m_ev_get = m_opt.GetIntValue("PRINT_EVENT_N");
+  m_curr_time = m_opt.GetStringValue("CURRENT_TIME");
+  m_col = m_opt.GetIntValue("COLUMN");
+  m_row = m_opt.GetIntValue("ROW");
+  m_thr = m_opt.GetIntValue("ADC_THREASHOLD");
+
+  m_adc_map = std::shared_ptr<TH2F>(new TH2F(Form("ADC map %s", m_curr_time), "ADC map", m_nx, 0, m_nx, m_ny, 0, m_ny));
+  m_adc_hist = std::shared_ptr<TH1F>(new TH1F(Form("ADC %s", m_curr_time), "ADC", 2000, -1000, 1000));
+}
+
+void GUIMonitor::Monitor(JadeDataFrameSP df)
+{
+  if (m_ev_get != 0 && m_ev_num % m_ev_get < 10) {
+    //df->Print(std::cout);
+    std::unique_lock<std::mutex> lk_in(m_mx_set);
+    m_df = df;
+    lk_in.unlock();
+
+    if ((m_ev_num) == 0) {
+      m_ev_num++;
+      return;
+    }
+
+    auto tr = new TRandom();
+    auto cds_adc = m_df->GetFrameCDS();
+    for (size_t iy = 0; iy < m_ny; iy++)
+      for (size_t ix = 0; ix < m_nx; ix++) {
+        auto pos = ix + m_nx * iy;
+        auto adc_value = cds_adc.at(pos);
+        auto ran_value = static_cast<int>(tr->Gaus(0, 300));
+        auto value = adc_value + ran_value;
+        if (std::abs(value) > m_thr) {
+          m_adc_map->Fill(ix, iy);
+        }
+        if (ix == m_col && iy == m_row) {
+          m_adc_hist->Fill(value);
+        }
+      }
+
+    std::unique_lock<std::mutex> lk_out(m_mx_get);
+    m_adc_map_clone = m_adc_map;
+    m_adc_hist_clone = m_adc_hist;
+    lk_out.unlock();
+  }
+  m_ev_num++;
+}
+
+void GUIMonitor::Reset()
+{
+}
+
+std::shared_ptr<TH2F> GUIMonitor::GetADCMap()
+{
+  return m_adc_map_clone;
+}
+
+std::shared_ptr<TH1F> GUIMonitor::GetADCHist()
+{
+  return m_adc_hist_clone;
+}
---
+++ software/gui/linux/src/mainwindow.cpp
@@ -0,0 +1,191 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget* parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+    , m_nx(48)
+    , m_ny(16)
+    , m_col(0)
+    , m_row(0)
+    , m_state("STOPPED")
+{
+  ui->setupUi(this);
+  this->setWindowIcon(QIcon("../software/gui/form/iheplogo.gif"));
+
+  m_GUIManager = new GUIManager();
+
+  connect(ui->Action_Open, SIGNAL(triggered()), this, SLOT(Action_Open_Triggered()));
+  connect(ui->Action_Save, SIGNAL(triggered()), this, SLOT(Action_Save_Triggered()));
+  connect(ui->Action_Exit, SIGNAL(triggered()), this, SLOT(Action_Exit_Triggered()));
+
+  ui->LineEdit_Online_FilePath->setText(QDir::currentPath());
+
+  connect(ui->Btn_Online_Choose, SIGNAL(clicked()), this, SLOT(Btn_Online_Choose_Clicked()));
+  connect(ui->Btn_Online_Config, SIGNAL(clicked()), this, SLOT(Btn_Online_Config_Clicked()));
+  connect(ui->Btn_Online_StartRun, SIGNAL(clicked()), this, SLOT(Btn_Online_StartRun_Clicked()));
+  connect(ui->Btn_Online_StopRun, SIGNAL(clicked()), this, SLOT(Btn_Online_StopRun_Clicked()));
+
+  m_col = ui->SpinBox_Online_Col->value();
+  m_row = ui->SpinBox_Online_Row->value();
+
+  connect(ui->Btn_Draw_Histogram, SIGNAL(clicked()), this, SLOT(Init_Online_Image()));
+
+  m_timer = new QTimer(this);
+  m_timer->setInterval(int(1e6 / ui->SpinBox_Online_evDisplay->value()));
+  connect(m_timer, SIGNAL(timeout()), this, SLOT(Update_Online_Image()));
+  connect(m_GUIManager, SIGNAL(IsRunning()), m_timer, SLOT(start()));
+  connect(m_GUIManager, SIGNAL(IsStop()), m_timer, SLOT(stop()));
+
+  m_timer_run = new QTimer(this);
+  m_timer_run->setInterval(int(ui->SpinBox_Online_TimeRun->value()));
+  connect(m_timer_run, SIGNAL(timeout()), this, SLOT(Btn_Online_StopRun_Clicked()));
+  connect(m_GUIManager, SIGNAL(IsRunning()), m_timer_run, SLOT(start()));
+  connect(m_GUIManager, SIGNAL(IsStop()), m_timer_run, SLOT(stop()));
+
+  m_root_timer = new QTimer(this);
+  m_root_timer->setInterval(int(1e6 / ui->SpinBox_Online_evDisplay->value()));
+  connect(m_root_timer, SIGNAL(timeout()), this, SLOT(Handle_Root_Events()));
+  connect(m_GUIManager, SIGNAL(IsRunning()), m_root_timer, SLOT(start()));
+}
+
+MainWindow::~MainWindow()
+{
+  delete ui;
+}
+
+void MainWindow::Action_Open_Triggered()
+{
+  QString filter = "File Description (*.dat *.df *.bin)";
+  auto files = QFileDialog::getOpenFileNames(this, "Open data Files", QDir::currentPath(), filter);
+  qDebug() << "Action_Open_Triggered... ";
+}
+
+void MainWindow::Action_Save_Triggered()
+{
+  QFileDialog dialog(this);
+  dialog.setWindowModality(Qt::WindowModal);
+  qDebug() << "Action_Save_Triggered... ";
+}
+
+void MainWindow::Action_Exit_Triggered()
+{
+  this->close();
+  qDebug() << "Action_Exit_Triggered... ";
+}
+
+void MainWindow::Btn_Online_Choose_Clicked()
+{
+  auto file_path = QFileDialog::getExistingDirectory(this, "Chose the directory to save data", QDir::currentPath());
+  ui->LineEdit_Online_FilePath->setText(file_path);
+  qDebug() << "Btn_Online_Choose_Clicked... ";
+}
+
+void MainWindow::Btn_Online_Config_Clicked()
+{
+  QString qinfile = ui->LineEdit_Online_InName->text();
+  m_GUIManager->set_input_data_path(qinfile.toStdString());
+
+  QString qoutfile = ui->LineEdit_Online_FilePath->text() + "/" + ui->LineEdit_Online_FileName->text();
+  m_GUIManager->set_output_data_path(qoutfile.toStdString());
+
+  QString qregfile = ui->LineEdit_Online_RegName->text();
+  m_GUIManager->set_register_data_path(qregfile.toStdString());
+
+  m_timer_run->setInterval(int(ui->SpinBox_Online_TimeRun->value()));
+  m_GUIManager->set_run_time(std::to_string(ui->SpinBox_Online_TimeRun->value()));
+
+  m_timer->setInterval(int(1e6 / ui->SpinBox_Online_evDisplay->value()));
+  m_GUIManager->set_ev_print(std::to_string(ui->SpinBox_Online_SampleEvents->value()));
+  m_GUIManager->set_chip_address(ui->SpinBox_Online_ChipAddress->value());
+  m_GUIManager->set_nfiles(ui->SpinBox_Online_NFiles->value());
+  m_GUIManager->set_channel(ui->SpinBox_Online_Col->value(), ui->SpinBox_Online_Row->value());
+  m_GUIManager->set_adc_threshold(ui->SpinBox_Online_ADCThreshold->value());
+
+  m_GUIManager->config();
+}
+
+void MainWindow::Btn_Online_StartRun_Clicked()
+{
+  std::cout << "=========GUI Start RUN: "
+            << "=======" << std::endl;
+
+  Clear_Online_Image();
+  ui->Btn_Online_StartRun->setEnabled(false);
+  ui->Btn_Online_StopRun->setEnabled(true);
+  m_state = "RUNNING";
+
+  m_GUIManager->start_run();
+
+  qDebug() << "Btn_Online_StartRun_Clicked... ";
+}
+
+void MainWindow::Btn_Online_StopRun_Clicked()
+{
+  ui->Btn_Online_StopRun->setEnabled(false);
+  m_GUIManager->stop_run();
+  std::cout << "=========GUI Stop RUN: "
+            << "=======" << std::endl;
+
+  ui->Btn_Online_StartRun->setEnabled(true);
+  qDebug() << "Btn_Online_StopRun_Clicked... ";
+}
+
+void MainWindow::Init_Online_Image()
+{
+  // Handle the "Draw Histogram" button clicked() event.
+
+  static TH1F* h1f = 0;
+  static TH2F* h2f = 0;
+
+  ui->Canvas_Plot->getCanvas()->Clear();
+  ui->Canvas_Plot->getCanvas()->cd();
+  ui->Canvas_Plot->getCanvas()->SetBorderMode(0);
+  ui->Canvas_Plot->getCanvas()->SetFillColor(0);
+  ui->Canvas_Plot->getCanvas()->SetGrid();
+  ui->Canvas_Plot->getCanvas()->Divide(1, 2);
+  ui->Canvas_Plot->getCanvas()->cd(1);
+
+  h1f = new TH1F("h1f", "Test random numbers", 4000, -2000, 2000);
+  h1f->SetFillColor(kViolet + 2);
+  h1f->SetFillStyle(3001);
+  h1f->Draw();
+
+  ui->Canvas_Plot->getCanvas()->cd(2);
+  h2f = new TH2F("h2f", "Test random numbers", 48, 0, 48, 16, 0, 16);
+  h2f->SetFillColor(kViolet + 2);
+  h2f->SetFillStyle(3001);
+  h2f->Draw();
+
+  ui->Canvas_Plot->getCanvas()->Modified();
+  ui->Canvas_Plot->getCanvas()->Update();
+}
+
+void MainWindow::Update_Online_Image()
+{
+  if (m_state == "RUNNING") {
+
+    ui->Canvas_Plot->getCanvas()->cd(1);
+    auto h1f = (TH1F*)m_GUIManager->get_monitor()->GetADCHist()->Clone("ADC");
+    h1f->SetFillColor(kViolet + 2);
+    h1f->SetFillStyle(3001);
+    h1f->Draw();
+
+    ui->Canvas_Plot->getCanvas()->cd(2);
+    auto hist2D = (TH2F*)m_GUIManager->get_monitor()->GetADCMap()->Clone("ADC MAP");
+    hist2D->Draw("COLZ");
+    ui->Canvas_Plot->getCanvas()->Modified();
+    ui->Canvas_Plot->getCanvas()->Update();
+  }
+}
+
+void MainWindow::Clear_Online_Image()
+{
+  connect(m_GUIManager, SIGNAL(IsRunning()), m_timer, SLOT(start()));
+  connect(m_GUIManager, SIGNAL(IsStop()), m_timer, SLOT(stop()));
+}
+
+void MainWindow::Handle_Root_Events()
+{
+  gSystem->ProcessEvents();
+}
